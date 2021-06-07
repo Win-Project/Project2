c68145c1ffdd110329b921cc7c47b692778c715a
cuteDazai <cutedazai@gmail.com>
2021-06-07 19:05:07 +0900

    리소스 수정 (다시)

--- resource.h
+++ resource.h
@@ -2,18 +2,17 @@
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
 // Resource.rc에서 사용되고 있습니다.
 //
-#define IDB_PNG1                        105
-#define IDB_BITMAP5                     108
-#define IDB_BITMAP4                     109
-#define IDB_BITMAP1                     110
-#define IDB_BITMAP2                     111
-#define IDB_BITMAP3                     112
+#define IDB_BITMAP1                     119
+#define IDB_BITMAP2                     120
+#define IDB_BITMAP3                     121
+#define IDB_BITMAP4                     122
+#define IDB_BITMAP5                     123
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        113
+#define _APS_NEXT_RESOURCE_VALUE        124
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
